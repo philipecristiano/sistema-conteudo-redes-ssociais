4efcaf63b97a967cd819230d15d0c84fcafb7c37
Philipe Cristiano <philipecristiano@gmail.com>
2025-05-24 00:09:09 -0300

    Update route.js

--- api/search/route.js
+++ api/search/route.js
@@ -18,7 +18,8 @@ export async function GET(request) {
   
   // Use o ID do mecanismo de pesquisa e a chave de API do Google
   const searchEngineId = '8002d35404ea24977';
-  const apiKey = process.env.GOOGLE_API_KEY || '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';
+  const apiKey = process.env.GOOGLE_API_KEY;
+
   
   try {
     const response = await fetch(
